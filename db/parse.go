02741b1de06e8414fc2dcd30047cb36640658059
romann <romann.belmer@gmail.com>
2024-12-06 14:35:13 +0100

    added parse Querys

--- db/parse.go
+++ db/parse.go
@@ -1,5 +1,19 @@
 package db
 
-func Parse(query string) {
+import (
+	"regexp"
+	"strings"
+)
 
+func ParseQuerys(query string) []string {
+	re := regexp.MustCompile(`SELECT\s+(.*?)\s+FROM`)
+	match := re.FindStringSubmatch(query)
+	if len(match) == 0 {
+		return nil
+	}
+	splited := strings.Split(match[1], ",")
+	for i, s := range splited {
+		splited[i] = strings.TrimSpace(s)
+	}
+	return splited
 }
