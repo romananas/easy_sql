9b034ab0e9ebe92dcc30d92ed95e86ca4fa33db9
CyberPangolin <74106192+romananas@users.noreply.github.com>
2024-12-06 17:50:20 +0100

    Add files via upload

--- queryrow.go
+++ queryrow.go
@@ -1,5 +1,19 @@
 package easy_sql
 
-func (d DB) QueryRow(q string, s any) {
-	keys := ParseQuerys(q)
+func (d DB) QueryRow(query string, dest any, args ...any) error {
+	keys := _ParseQuerys(query)
+
+	ptrs, err := _GetPointers(dest)
+	tags := _ReadTags(dest)
+	names := _ReadNames(dest)
+	if err != nil {
+		return err
+	}
+	order := _SortKeys(tags, names, keys)
+	var sorted []any
+	for _, i := range order {
+		sorted = append(sorted, ptrs[i])
+	}
+	ptrs = sorted
+	return d.db.QueryRow(query, args...).Scan(ptrs...)
 }
