f8ab2c23d6eba50d68f0fd65b13c0021ec2fecd9
romann <romann.belmer@gmail.com>
2024-12-06 14:42:53 +0100

    added parse Querys

--- queryrow.go
+++ queryrow.go
@@ -1,7 +1,5 @@
 package easy_sql
 
-import "github.com/romananas/easy_sql/utils"
-
 func (d DB) QueryRow(q string, s any) {
-	keys := utils.ParseQuerys(q)
+	keys := ParseQuerys(q)
 }
--- utils/parse.go
+++ utils/parse.go
@@ -1,4 +1,4 @@
-package utils
+package easy_sql
 
 import (
 	"regexp"
